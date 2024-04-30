3591c49b51d5a6253da3fa4245eef60bcaae7c1f
Usuario <Usuario@CPD03-M03>
2024-04-30 10:44:24 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = { "JOÃO", "PEDRO", "MARIA" };
+
+            Console.WriteLine("Digite um número de 0 a 2: ");
+
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O nome selecionado foi: " +nome[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
